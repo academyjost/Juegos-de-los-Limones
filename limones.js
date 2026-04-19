4ebab76cfe046a63aff6a9cdef35fb001a772166
Jost <academyjost@gmail.com>
2026-04-19 10:35:31 -0500

    git add . git commit -m "Agregando el juego de atrapar limones, con su funcionalidad básica y estilo."

--- limones.js
+++ limones.js
@@ -1,2 +1,19 @@
 let canvas = document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
+
+const Altura_Suelo=40;
+const Altura_Personaje=60;
+const Ancho_Personaje=40;
+
+function iniciar(){
+    dibujarSuelo();
+    dibujarPersonaje();
+}
+function dibujarSuelo(){
+    ctx.fillStyle="#442165";
+    ctx.fillRect(0,canvas.height-Altura_Suelo,canvas.width,Altura_Suelo);
+}
+function dibujarPersonaje(){
+    ctx.fillStyle="#1900ff";
+    ctx.fillRect(canvas.width/2,canvas.height-(Altura_Suelo+Altura_Personaje),Ancho_Personaje,Altura_Personaje);
+}
